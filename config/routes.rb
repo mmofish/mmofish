96e7d3335e84a424c864dfe5d03f1bac741c1fbe
Martin Dean <mizpah@enigmaguild.org>
2013-03-01 23:58:00 +0000

    Add sign-up Stub and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Mmofish::Application.routes.draw do
-  
+
   root :to => 'static_pages#home'
+
+  match '/signup', to: 'users#new'
+  
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
-  
-  get "static_pages/home"
-
-  
+      
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
