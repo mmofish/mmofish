76390a7aa43ccf3b0ae29743d5ad524a08c301d6
Martin Dean <mizpah@enigmaguild.org>
2013-03-08 14:09:05 +0000

    yet more changes

--- app/controllers/admin/settings/users_controller.rb
+++ app/controllers/admin/settings/users_controller.rb
@@ -1,7 +1,9 @@
 #controllers/admin/settings/users_controller.rb
 
 class Admin::Settings::UsersController < Admin::DashboardController
+
   def index
+ 
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
     @users = User.all
   end
