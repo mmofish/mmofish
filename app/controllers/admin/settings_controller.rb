b6e34aa6431f0565759d0f8ce7411aa141b16e66
Martin Dean <mizpah@enigmaguild.org>
2013-03-09 22:00:45 +0000

    settings

--- app/controllers/admin/settings_controller.rb
+++
@@ -1,8 +0,0 @@
-class Admin::SettingsController < Admin::DashboardController
-  def index
-      @users = User.all
-        respond_to do |format|
-          format.html # index.html.erb
-       end
-  end
-end
